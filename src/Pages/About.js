a0ba82e611f9364bc08f193a256a3d9e70093120
Paul_Ajibola <paulajibola6@gmail.com>
2025-10-23 18:05:58 +0100

    cv update for health tech job application

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -88,7 +88,7 @@ export default class About extends Component {
 									</b>
 								</p>
 								<button className="button2">
-									<a href="https://drive.google.com/file/d/1MHAryYUj3KtcE5RUEifOVlFsh_Ks53zC/view?usp=sharing" 
+									<a href="https://drive.google.com/file/d/1MHIu3NtBKHfosSrQ4uIi22gLbHGV4EqJ/view?usp=sharing" 
 									target="_blank"
 									rel="noopener noreferrer"
 									download="PaulAJIBOLA_Resume.pdf">Download My Resume</a>
